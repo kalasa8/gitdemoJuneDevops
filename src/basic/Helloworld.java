74913288b29364b9bd97a523c0dd11716b8db382
kalasa8 <shanthikalakota@gmail.com>
2024-06-20 19:03:31 -0500

    adding numbers

--- src/basic/Helloworld.java
+++ src/basic/Helloworld.java
@@ -6,6 +6,9 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Hello world");
+ int a=10;
+ int b=20;
+ System.out.println(a+b);
 	}
 
 }
